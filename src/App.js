be56ef9f60ca04d6b37f971ceb7a1550d070e42b
yasupeter <peterleeful@gmail.com>
2023-10-31 15:24:33 +0900

    prettier 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-
 function App() {
+  // 1~100
+  const number = Math.ceil(Math.random() * 100);
+  const isLarge = number > 50;
+  const isSmall = number <= 50;
+
   return (
-    <div>
-      <h1>hello react</h1>
-        <h2>HI REACT</h2>
-        <MyComp />
-    </div>
+    <>
+      <h1>난수 : {number}</h1>
+      <h1>{number > 50 ? "큰 수" : "작은 수"}</h1>
+      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
+      <div>{isLarge && <h1>큰 수다!!</h1>}</div>
+      <div>{isLarge || <h1>작은 수다!!</h1>}</div>
+    </>
   );
 }
 
-// built-in component는 소문자로 시작
-// component 명은 꼭 대문자로 시작
-// return 문을 가져야함
-// return 값은 보통 JSX 코드
-
-
-function MyComp() {
-    // return 에 jsx 코드 작성 가능
-    // 꼭 하나의 root tag가 존재해야 함
-    // return 값이 여러 줄이면 ()로 감싸기
-    // root tag가 필요없다면 fragment 사용 가능
-
-    // jsx에서 주석 사용법
-    // {/* */}
-    return (
-        <>
-            {/*주석은 이렇게 다세요.*/}
-            {/* fragment는 <></> */}
-            <h1>HI COMPONENT</h1>
-            <h2>hello</h2>
-        </>
-    )
+function BigImage() {
+  return <h1>큰 이미지</h1>;
 }
+
+function SmallImage() {
+  return <h3>작은 이미지</h3>;
+}
+
 export default App;
