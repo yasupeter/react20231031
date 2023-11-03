f2b7b46d9b79fe137fa5cab96dbac2b626bc8053
yasupeter <peterleeful@gmail.com>
2023-11-03 12:35:54 +0900

    Fri, 11.3 event.preventDefault() : 기본기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
-function MyComponent1() {
-    return (
-        <div>
-            <h1>Hello Component</h1>
-        </div>
-    )
-}
-
-function MyComponent2() {
-    return (
-        <>
-            {/*p>lorem5^ul>li*3>lorem1*/}
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-                <li>Lorem.</li>
-                <li>Labore.</li>
-                <li>Quas?</li>
-            </ul>
-        </>
-    );
-}
-
 function App() {
-    return (
-        <div>
-            <MyComponent1 />
-            {/*
-      <div>
-        <h1>Hello Component</h1>
-      </div>
-      */}
-            <MyComponent2 />
-            {/*
-        <p>lorem ipsum alkdjf alkdjf </p>
-        <ul>
-          <li>lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-        </ul>
-       */}
-        </div>
-    );
+  function handleClick(e) {
+    // a 태그 클릭시 naver로 이동할 수 있게 메소드 작성
+
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+  ``;
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
-
-
 
 export default App;
