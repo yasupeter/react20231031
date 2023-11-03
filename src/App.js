025e98b3dbe52657ce17b603539e3c52bac869a0
yasupeter <peterleeful@gmail.com>
2023-11-03 15:34:35 +0900

    Fri, 11.3 예제) state를 가공해서 쓸 수 있음

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Center } from "@chakra-ui/react";
 
-function App(props) {
-  let [number, setNumber] = useState(0);
-
-  function handleClick() {
-    // number++;
-    setNumber(number + 1);
+function App() {
+  function handleOuterBoxClick() {
+    console.log("바깥 상자 클릭됨");
+  }
+  function handleInnerBoxClick() {
+    console.log("안쪽 상자 클릭됨");
+  }
+  function handleButtonClick() {
+    console.log("버튼 클릭됨");
   }
   return (
-    <div>
-      <Button onClick={handleClick}>바꾸기</Button>
-      <Text>{number}</Text>
-    </div>
+    <>
+      <Center onClick={handleOuterBoxClick}>
+        <Center onClick={handleInnerBoxClick}>
+          <Button onClick={handleButtonClick}>button</Button>
+        </Center>
+      </Center>
+    </>
   );
 }
 
