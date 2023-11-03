e52a730ffb37f8fef38223c427ad8d5d7ea77230
yasupeter <peterleeful@gmail.com>
2023-11-03 10:25:42 +0900

    Fri, 11.3 chakra ui, chakra ui 수정

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import { Card, CardBody } from "@chakra-ui/react";
 
 function MyBox({ color, bg, children }) {
-  return (
-    <>
-      <Card color={color} bg={bg}>
-        <CardBody>{children}</CardBody>
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <Card color={color} bg={bg}>
+                <CardBody>{children}</CardBody>
+            </Card>
+        </>
+    );
 }
 function App(props) {
-  return (
-    <div>
-      <MyBox color="teal" bg="gray">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
-        architecto blanditiis commodi delectus eligendi fuga harum impedit
-        maxime, necessitatibus nostrum odit, pariatur praesentium quae quaerat
-        quas qui veniam? Dolorem, in.
-      </MyBox>
-      <MyBox color="blue" bg="pink">
-        HELLO WORLD
-      </MyBox>
-    </div>
-  );
+    return (
+        <div>
+            <MyBox color="teal" bg="gray">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                architecto blanditiis commodi delectus eligendi fuga harum impedit
+                maxime, necessitatibus nostrum odit, pariatur praesentium quae quaerat
+                quas qui veniam? Dolorem, in.
+            </MyBox>
+            <MyBox color="blue" bg="pink">
+                HELLO WORLD
+            </MyBox>
+        </div>
+    );
 }
 
 export default App;
