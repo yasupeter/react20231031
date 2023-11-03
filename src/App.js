ed2319db6233fead5d3ad1fca1ccbc5e477316b0
yasupeter <peterleeful@gmail.com>
2023-11-03 12:32:34 +0900

    Fri, 11.3 함수를 props으로 전달, 완료

--- src/App.js
+++ src/App.js
@@ -1,30 +1,4 @@
-import React from "react";
+function App()
 
-function App(props) {
-  function handleClick(e) {
-    // 브라우저가 해야하는 기본 기능을 취소함
-    e.preventDefault();
-    console.log("다른 일을 시킴");
-  }
-
-  function handleSubmit(e) {
-    // 기본 기능 취소
-    e.preventDefault();
-    console.log("다른일을 시킴");
-  }
-  return (
-    <div>
-      <a href="https://www.naver.com" onClick={handleClick}>
-        네이버
-      </a>
-      <div>
-        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
-          <input type="text" name="q" />
-          <button>검색</button>
-        </form>
-      </div>
-    </div>
-  );
-}
 
 export default App;
