1d791ea75e0ba1aa6a9505322e13a1aa104569de
yasupeter <peterleeful@gmail.com>
2023-11-03 16:19:23 +0900

    Fri, 11.3 state를 변경해서 input의 value 할당하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { Button, Center } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Button } from "@chakra-ui/react";
 
-function App() {
-  function handleOuterBoxClick() {
-    console.log("바깥 상자 클릭됨");
+function App(props) {
+  const [number, setNumber] = useState(0);
+
+  function handlePlusButtonClick() {
+    setNumber(number + 1);
   }
-  function handleInnerBoxClick() {
-    console.log("안쪽 상자 클릭됨");
+
+  function handleMinusButtonClick() {
+    setNumber(number - 1);
   }
-  function handleButtonClick() {
-    console.log("버튼 클릭됨");
-  }
+
   return (
-    <>
-      <Center onClick={handleOuterBoxClick}>
-        <Center onClick={handleInnerBoxClick}>
-          <Button onClick={handleButtonClick}>button</Button>
-        </Center>
-      </Center>
-    </>
+    <div>
+      <input type="number" value={number} />
+      <Box>
+        <Button onClick={handlePlusButtonClick}>+</Button>
+        <Button onClick={handleMinusButtonClick}>-</Button>
+      </Box>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>+</Button>
+        <Button onClick={() => setNumber(number - 1)}>-</Button>
+      </Box>
+    </div>
   );
 }
 
