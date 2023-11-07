76a81af9e398df7eacf61aa66bc60a3d2d51edea
yasupeter <peterleeful@gmail.com>
2023-11-07 17:38:57 +0900

    Tues, 11.7 state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,47 +1,84 @@
-function MyComponent1() {
-    return (
-        <div>
-            <h1>Hello Component</h1>
-        </div>
-    )
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+function App(props) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  function handleNameInput(e) {
+    setName(e.target.value);
+  }
+  function handlePasswordInput(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleEmailInput(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleSubmit() {
+    setSubmitting(true);
+    /*
+        axios
+          .post("/adduser", {
+            name,
+            email,
+            password,
+          })
+          .then((response) => console.log("성공할 때 해야하는일"))
+          .catch((error) => console.log("실패할 때 해야하는 일"))
+          .finally(() => console.log("꼭 해야하는 일"));
+
+         */
+  }
+
+  return (
+    <Center>
+      <Box w={"480px"}>
+        <FormControl mb={5}>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" value={name} onChange={handleNameInput} />
+          <FormHelperText>띄어쓰기 없이 입력해주세요.</FormHelperText>
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={handlePasswordInput}
+          />
+          <FormHelperText>
+            특수기호와 숫자를 하나 이상 작성해주세요.
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl mb={5}>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" value={email} onChange={handleEmailInput} />
+          <FormHelperText>입력된 이메일은 중복될 수 없습니다.</FormHelperText>
+        </FormControl>
+
+        <Button
+          onClick={handleSubmit}
+          colorScheme="blue"
+          isLoading={submitting}
+        >
+          가입
+        </Button>
+      </Box>
+    </Center>
+  );
 }
-
-function MyComponent2() {
-    return (
-        <>
-            {/*p>lorem5^ul>li*3>lorem1*/}
-            <p>Lorem ipsum dolor sit amet.</p>
-            <ul>
-                <li>Lorem.</li>
-                <li>Labore.</li>
-                <li>Quas?</li>
-            </ul>
-        </>
-    );
-}
-
-function App() {
-    return (
-        <div>
-            <MyComponent1 />
-            {/*
-      <div>
-        <h1>Hello Component</h1>
-      </div>
-      */}
-            <MyComponent2 />
-            {/*
-        <p>lorem ipsum alkdjf alkdjf </p>
-        <ul>
-          <li>lorem</li>
-          <li>ipsum</li>
-          <li>dolor</li>
-        </ul>
-       */}
-        </div>
-    );
-}
-
-
 
 export default App;
